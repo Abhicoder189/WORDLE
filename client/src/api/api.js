c0a46a39074e0ee603c0ffe4fef4c38bd87b1954
Abhicoder189 <johndoe@example.com>
2026-03-26 21:15:23 +0530

    setup for frontend deployment

--- client/src/api/api.js
+++ client/src/api/api.js
@@ -1,16 +1,16 @@
 import axios from "axios"
-
+const API_URL = "https://wordle-api-abhi.onrender.com";
 const fetchWord =async () =>{
-    const response = await axios.get("http://localhost:8000/api/game")
+    const response = await axios.get(`${API_URL}/api/game`)
     return response.data
 
 }
 const submitGuess=async (guess,word)=>{
-  const response=  await axios.post("http://localhost:8000/api/game" ,{guess,word})
+  const response=  await axios.post(`${API_URL}/api/game` ,{guess,word})
   return response.data
 }
 const saveScore=async(score)=>{
-   const response=await axios.post("http://localhost:8000/api/scores",score)
+   const response=await axios.post(`${API_URL}/api/scores`,score)
    return response.data
 
 }
