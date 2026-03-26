87ebccd8de9d75088c817059bad6100ab8d4d692
Abhicoder189 <johndoe@example.com>
2026-03-27 03:50:34 +0530

    added virtual keyboard component and added functionality to the input field to allow for the virtual keyboard to be used. also added functionality to the virtual keyboard to allow for the input field to be updated with the virtual keyboard input.

--- client/src/components/Keyboard.jsx
+++ client/src/components/Keyboard.jsx
@@ -1,26 +1,20 @@
-import React, { useState, useEffect } from 'react';
-import "./Keyboard.css"
 const Keyboard = ({ currentGuess, onKeyPress }) => {
+  const keys = ["Q", "W", "E", "R", "T", "Y", "U", "I", "O", "P", "A", "S", "D", "F", "G", "H", "J", "K", "L", "Z", "X", "C", "V", "B", "N", "M", "Backspace", "Enter"];
 
-  // useEffect with keydown listener
-    useEffect(() => {
-        const handleKeyDown = (event) => {
-            onKeyPress(event.key);
-        }
-
-        window.addEventListener('keydown', handleKeyDown);
-
-        return () => {
-            window.removeEventListener('keydown', handleKeyDown);
-        };
-    }, [onKeyPress]);
-
-
-  // return display of currentGuess
   return (
-    <div className="keyboard">
+    <div className="keyboard-container">
       <div className="current-guess">{currentGuess}</div>
+      <div className="visual-keyboard">
+        {keys.map((key) => (
+          <button 
+            key={key} 
+            onClick={() => onKeyPress(key)}
+            className="key-button"
+          >
+            {key}
+          </button>
+        ))}
+      </div>
     </div>
   );
-}
-export default Keyboard;
+};
